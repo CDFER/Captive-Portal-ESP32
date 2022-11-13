862fc71c97cb0f7005aba4fec424ab589da90bfc
CDFER <cdirksfer@gmail.com>
2022-11-13 13:45:29 +1300

    Add Caching HTML (Tested on Desktop), Tidy up

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
-# ESP Cross Platform Captive Portal
+# ESP Cross Platform Captive Portal 
+
 A ESP32 Captive Portal example that actually works on all devices (or that is the goal).
-When you connect to the wifi "captive" is should take you straight to http://4.3.2.1/ and show the text "Hooray"
+When you connect to the wifi "captive" password "12345678" it should take you straight to http://4.3.2.1/ and show the text "Hooray"
 
-
-I need you help! If you test this code on a device (even if it works)
-Fill out the form here: https://forms.gle/ArLPTnwRA3QGTKyc6
+I need you help! If you test this code on a device (even if it works) fill out the form here: https://forms.gle/ArLPTnwRA3QGTKyc6
 
 A list of all tested devices can be seen here: https://docs.google.com/spreadsheets/d/1588-0fG21-aaR2pWJNQjufwgPaIDbHX_0geHEn1yDHk/edit?usp=sharing
 
+## Known Bugs at the moment
 
-Known Limitations at the moment:
----------------
-- Firefox gives a warning page about going to a non-https page
-- Oneplus 6 (Oxygen OS 11) gets stuck on setting client side IP address sometimes when cellular is on
+- Oneplus 6 phone (Oxygen OS 11) gets stuck on setting client side IP address sometimes when cellular is on
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <Arduino.h>
 #include <DNSServer.h>
-#include <esp_wifi.h>
+#include <esp_wifi.h> //Used for android work around
 #include <WiFi.h>
-#include <AsyncTCP.h>
-#include "ESPAsyncWebServer.h"
+#include <AsyncTCP.h> 	//https://github.com/me-no-dev/AsyncTCP using the latest dev version from @me-no-dev
+#include "ESPAsyncWebServer.h" //ESP Async WebServer using the latest stable version from @me-no-dev
 
+// Dependency Graph
+// |-- AsyncTCP @ 1.1.1+sha.ca8ac5f
+// |-- ESP Async WebServer @ 1.2.3
+// |   |-- AsyncTCP @ 1.1.1+sha.ca8ac5f
+// |   |-- FS @ 2.0.0
+// |   |-- WiFi @ 2.0.0
+// |-- DNSServer @ 2.0.0
+// |   |-- WiFi @ 2.0.0
+// |-- WiFi @ 2.0.0
 
 //Pre reading on the fundamentals of captive portals https://textslashplain.com/2022/06/24/captive-portals/
 
@@ -12,14 +21,14 @@ DNSServer dnsServer;
 AsyncWebServer server(80);
 
 void setup(){ //the order of the code is really important and it is critical the the android workaround is after the dns and sofAP setup
-  Serial.begin(460800);
+  Serial.begin(115200);
   Serial.print("\n");
 
   WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(IPAddress(4, 3, 2, 1), IPAddress(4, 3, 2, 1), IPAddress(255, 255, 255, 0)); //samusung requires the IP to be in public space
-  WiFi.softAP("captive", "12345678", 6, 0, 4); //2.4ghz channel 6, broadcast ssid (0), 4 max clients, FYI The SSID in SoftAP can't have a space in it.
+  WiFi.softAPConfig(IPAddress(4, 3, 2, 1), IPAddress(4, 3, 2, 1), IPAddress(255, 255, 255, 0)); //Samusung requires the IP to be in public space
+  WiFi.softAP("captive", "12345678", 6, 0, 4); //2.4ghz channel 6, do broadcast SSID (0), 4 max clients, FYI The SSID in SoftAP can't have a space in it.
   
-  dnsServer.setErrorReplyCode(DNSReplyCode::NoError); //not sure if this is neccacary
+  dnsServer.setErrorReplyCode(DNSReplyCode::NoError); //not sure if this is necessary
   dnsServer.setTTL(300); //set 5min client side cache for DNS
   dnsServer.start(53, "*", WiFi.softAPIP());
 
@@ -38,12 +47,12 @@ void setup(){ //the order of the code is really important and it is critical the
   //Required
   server.on("/connecttest.txt",[](AsyncWebServerRequest *request){request->redirect("http://logout.net/");}); //windows 11 captive portal workaround
 
-  //Probabilly not Required, but might speed things up?
+  //Probably not Required, but might speed things up?
   server.on("/canonical.html",[](AsyncWebServerRequest *request){request->redirect("http://4.3.2.1/");}); //firefox captive portal call home
   server.on("/chrome-variations/seed",[](AsyncWebServerRequest *request){request->send(200);}); //chrome captive portal call home
   server.on("/redirect",[](AsyncWebServerRequest *request){request->redirect("http://4.3.2.1/");}); //microsoft redirect
   server.on("/success.txt",[](AsyncWebServerRequest *request){request->send(200);}); //firefox captive portal call home
-  server.on("/wpad.dat",[](AsyncWebServerRequest *request){request->send(404);}); //Honestly don't understand what this is but a 404 stops win 10 keep calling this repetededly and panicing the esp32 :)
+  server.on("/wpad.dat",[](AsyncWebServerRequest *request){request->send(404);}); //Honestly don't understand what this is but a 404 stops win 10 keep calling this repeatedly and panicking the esp32 :)
   server.on("/generate_204",[](AsyncWebServerRequest *request){request->redirect("http://4.3.2.1/");}); //chromium? android? captive portal redirect
   server.on("/service/update2/json",[](AsyncWebServerRequest *request){request->send(200);}); //firefox?
   server.on("/chat",[](AsyncWebServerRequest *request){request->send(404);}); //No stop asking Whatsapp, there is no internet connection
@@ -53,14 +62,16 @@ void setup(){ //the order of the code is really important and it is critical the
 
   //Serve Basic HTML Page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/html", "<!DOCTYPE html><html><head><title>Success</title></head><body><p>Hooray</p></body>");
-    Serial.println("Served Basic HTML Page");
+    AsyncWebServerResponse *response = request->beginResponse(200, "text/html", "<!DOCTYPE html><html><head><title>Success</title></head><body><p>Hooray</p></body>");
+    response->addHeader("Cache-Control", "public,max-age=31536000");
+    request->send(response);
+    Serial.println("Served Basic HTML Page with 7 day Cache header");
   });
 
   server.onNotFound([](AsyncWebServerRequest *request){
     request->redirect("http://4.3.2.1/");
     Serial.print("server.onnotfound ");
-    Serial.print(request->host());       //This gives some insight into whatever was being requested
+    Serial.print(request->host());       //This gives some insight into whatever was being requested on the serial monitor
     Serial.print(request->url());
     Serial.print(" sent redirect to http://4.3.2.1/\n");
   });
